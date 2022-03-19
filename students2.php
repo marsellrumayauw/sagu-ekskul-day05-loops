5a0391d3aa7fcb865c50341d07358ff16fbfe53c
marsellrumayauw <rumayauw98@gmail.com>
2022-03-19 13:46:39 +0900

    new one

---
+++ students2.php
@@ -0,0 +1,8 @@
+<?php
+    $studentsstudents=array(
+        "Beri Pahabol" =>"19",
+        "Marina Rumi" =>"17",
+        "Yekson Bagau" =>"19",
+        "Stevania Wanggai" =>"19"
+    )
+?>
---
+++ students3.php
@@ -0,0 +1,6 @@
+<?php
+    $students=array(
+        array("Beri Pahabol", 19, "beri.pahabol@gmail.com"),
+        array("Marin Rumi", 17, "marina.rumi@gmail.com")
+    )
+?>
